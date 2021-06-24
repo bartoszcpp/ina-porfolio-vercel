ec86704b62e0f0e2275695e512d695992e7d6f16
bartoszcpp <bartosz.cp@gmail.com>
2021-06-24 19:23:44 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/pages/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Footer() {
+  return <Footer>footer</Footer>
+}
+
+export default Footer
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "bootstrap/dist/css/bootstrap.css"
+import "./index.css"
+import Header from "./components/Header"
+import Main from "./components/Main"
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
+}
